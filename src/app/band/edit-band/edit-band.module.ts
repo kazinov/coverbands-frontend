f14706db1ce508ebf3ddba28a9bf1e559a0c00da
kazinov <dm.kazinov@gmail.com>
2019-04-25 16:38:12 +0200

    accordion

--- src/app/band/edit-band/edit-band.module.ts
+++ src/app/band/edit-band/edit-band.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainBandInfoModule } from '../main-band-info/main-band-info.module';
 import { EditBandComponent } from './edit-band.component';
-import { MatTabsModule } from '@angular/material';
+import { MatExpansionModule } from '@angular/material';
 import { CoversInfoModule } from '../covers-info/covers-info.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MainBandInfoModule,
-    MatTabsModule,
-    CoversInfoModule
+    CoversInfoModule,
+    MatExpansionModule
   ],
   declarations: [
     EditBandComponent
