078f01aa0985f593da6c3efe4965778950c308bc
kazinov <dm.kazinov@gmail.com>
2019-06-07 11:32:46 +0200

    prices

--- src/app/band/band-prices/band-prices.component.ts
+++ src/app/band/band-prices/band-prices.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 import { BandPrices, Price } from './band-prices.model';
+import { Currencies } from '../../core/currencies/currencies.model';
 
 @Component({
   selector: 'app-band-prices',
@@ -15,51 +16,77 @@ export class BandPricesComponent implements OnInit {
   form: FormGroup;
   @ViewChild('formGroup') formGroup: FormGroupDirective;
   tableColumns: string[] = ['value', 'service', 'remove'];
+  pricesDirty: Price[] = [];
 
   ngOnInit() {
     this.buildForms();
   }
 
   private buildForms(): void {
+    this.pricesDirty = this.prices && this.prices.prices || [];
     this.addPriceFrom = this.formBuilder.group({
       value: ['', [Validators.required]],
       service: ['', []]
     });
 
     this.form = this.formBuilder.group({
-      value: ['', [Validators.required]],
-      service: ['', []]
+      value: [
+        this.prices &&
+        this.prices.oneShowPrice &&
+        this.prices.oneShowPrice.value, []],
+      service: [this.prices &&
+      this.prices.oneShowPrice &&
+      this.prices.oneShowPrice.service, []]
     });
   }
 
   onPriceSubmit() {
-    this.prices.prices = [
-      ...(this.prices.prices || []),
+    this.pricesDirty = [
+      ...(this.pricesDirty || []),
       this.addPriceFrom.value
     ];
 
-    // this.pricesChange.emit(this.prices);
     this.addPriceFrom.reset();
     this.formGroup.resetForm();
   }
 
   onLinkDelete(linkToDelete: Price) {
-    this.prices.prices = (this.prices.prices || [])
+    this.pricesDirty = (this.pricesDirty || [])
       .filter((price) => {
         return !(price.value === linkToDelete.value
           && price.service === linkToDelete.service);
       });
-    // this.pricesChange.emit(this.prices);
   }
 
   get pricesExist() {
-    return this.prices && this.prices.prices && this.prices.prices.length;
+    return this.pricesDirty && this.pricesDirty.length;
   }
 
   onSubmit() {
-    console.log('onSubmit')
-  }
+    const result: BandPrices = {
+      oneShowPrice: null,
+      prices: null
+    };
 
+    if (this.form.value.value) {
+      result.oneShowPrice = {
+        value: this.form.value.value,
+        currency: this.form.value.value ? Currencies.RussianRuble : undefined,
+        service: this.form.value.service
+      }
+    }
+
+    if (this.pricesDirty.length) {
+      result.prices = this.pricesDirty
+        .map((price: Price) => ({
+          value: price.value,
+          currency: Currencies.RussianRuble,
+          service: price.service
+        } as Price))
+    }
+
+    this.pricesChange.emit(result);
+  }
 
   constructor(
     private formBuilder: FormBuilder
