a509447efd3de00114c049dbc7dff209703b000d
kazinov <dm.kazinov@gmail.com>
2019-07-03 12:11:16 +0200

    images uploader styling

--- src/app/shared/images-uploader/images-uploader.module.ts
+++ src/app/shared/images-uploader/images-uploader.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagesUploaderComponent } from './images-uploader.component';
 import { FileUploadButtonModule } from '../file-upload-button/file-upload-button.module';
+import { MatIconModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    FileUploadButtonModule
+    FileUploadButtonModule,
+    MatIconModule
   ],
   declarations: [ImagesUploaderComponent],
   exports: [ImagesUploaderComponent]
