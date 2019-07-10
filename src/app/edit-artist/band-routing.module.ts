cd5c7bbd04f31202a817661e9555745b64410b8b
kazinov <dm.kazinov@gmail.com>
2019-07-10 10:17:43 +0200

    rename

--- src/app/edit-artist/band-routing.module.ts
+++ src/app/edit-artist/band-routing.module.ts
@@ -2,7 +2,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BandPaths } from './band-paths';
 import { CreateBandComponent } from './create-band/create-band.component';
-import { EditBandComponent } from './edit-band/edit-band.component';
+import { EditBandComponent } from './edit-artist/edit-band.component';
 
 export const bandRoutes: Routes = [
   {path: '', redirectTo: BandPaths.Create, pathMatch: 'full'},
--- src/app/edit-artist/band.module.ts
+++ src/app/edit-artist/band.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BandRoutingModule } from './band-routing.module';
 import { CommonModule } from '@angular/common';
 import { CreateBandModule } from './create-band/create-band.module';
-import { EditBandModule } from './edit-band/edit-band.module';
+import { EditBandModule } from './edit-artist/edit-band.module';
 
 @NgModule({
   imports: [
