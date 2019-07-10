1a5adc327be91ab58d8e5fddd77bd63198f8942c
kazinov <dm.kazinov@gmail.com>
2019-07-10 10:36:47 +0200

    rename

--- src/app/artist-admin/artist-admin-routing.module.ts
+++ src/app/artist-admin/artist-admin-routing.module.ts
@@ -4,7 +4,7 @@ import { EditArtistComponent } from './edit-artist/edit-artist.component';
 import { CreateArtistComponent } from './create-artist/create-artist.component';
 import { ArtistAdminPaths } from './artist-admin-paths';
 
-export const bandRoutes: Routes = [
+export const artistAdminRoutes: Routes = [
   {path: '', redirectTo: ArtistAdminPaths.Create, pathMatch: 'full'},
   {
     path: ArtistAdminPaths.Create,
@@ -17,7 +17,7 @@ export const bandRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(bandRoutes)]
+  imports: [RouterModule.forChild(artistAdminRoutes)]
 })
 export class ArtistAdminRoutingModule {
 }
