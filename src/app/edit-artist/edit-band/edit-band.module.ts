0f4a2fe0fc4fc4963cc103be8a27de080f112968
kazinov <dm.kazinov@gmail.com>
2019-07-10 09:53:43 +0200

    rename

--- src/app/edit-artist/edit-band/edit-band.module.ts
+++ src/app/edit-artist/edit-band/edit-band.module.ts
@@ -4,11 +4,11 @@ import { MainBandInfoModule } from '../main-band-info/main-band-info.module';
 import { EditBandComponent } from './edit-band.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { CoversInfoModule } from '../covers-info/covers-info.module';
 import { EditBandImagesModule } from '../edit-band-images/edit-band-images.module';
 import { EditArtistContactsModule } from '../edit-artist-contacts/edit-artist-contacts.module';
 import { EditArtistLinksModule } from '../edit-artist-links/edit-artist-links.module';
 import { EditArtistPricesModule } from '../edit-artist-prices/edit-artist-prices.module';
+import { CoversInfoModule } from '../edit-artist-covers/covers-info.module';
 
 @NgModule({
   imports: [
