17cdcd98c0691e85b92b654ec70b66c40a9b3baa
kazinov <dm.kazinov@gmail.com>
2019-07-19 12:34:37 +0200

    validate video url

--- src/app/admin/artist-admin/edit-artist/edit-artist-videos/video-embed-format.validator.ts
+++ src/app/admin/artist-admin/edit-artist/edit-artist-videos/video-embed-format.validator.ts
@@ -1,8 +1,19 @@
 import { FormControl } from '@angular/forms';
 import { parseEmbeddedVideoSrc } from '@shared/utils/parse-embedded-video-src';
 
+const ALLOWED_SOURCES: RegExp[] = [
+  /^https:\/\/www\.youtube\.com\/embed\/+./i,
+  /^\/\/vk\.com\/video_ext\.php+./i,
+  /^\/\/rutube\.ru\/play\/embed\/+./i,
+  /^https:\/\/player\.vimeo\.com\/video\/+./i,
+  /^https:\/\/www\.facebook\.com\/plugins\/video\.php+./i,
+  /^\/\/ok\.ru\/videoembed\/+./i
+];
+
 export function videoEmbedFormatValidator(control: FormControl) {
-  if (!!parseEmbeddedVideoSrc(control.value)) {
+  const url = parseEmbeddedVideoSrc(control.value)
+  if (!!url
+    && ALLOWED_SOURCES.some((source: RegExp) => source.test(url))) {
     return null;
   } else {
     return {invalidVideoEmbedFormat: true};
