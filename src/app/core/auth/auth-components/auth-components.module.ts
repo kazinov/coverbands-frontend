d1ebac6ed307a270cdfb9cfb11d904361ea007f5
kazinov <dm.kazinov@gmail.com>
2019-07-26 11:36:57 +0200

    sign in

--- src/app/core/auth/auth-components/auth-components.module.ts
+++ src/app/core/auth/auth-components/auth-components.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { AuthDialogComponent } from '@core/auth/auth-components/auth-dialog/auth-dialog.component';
 import { MatButtonModule, MatDialogModule, MatIconModule, MatProgressSpinnerModule } from '@angular/material';
 import { CommonModule } from '@angular/common';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 @NgModule({
   imports: [
@@ -13,7 +14,7 @@ import { CommonModule } from '@angular/common';
   ],
   providers: [],
   bootstrap: [],
-  declarations: [AuthDialogComponent],
+  declarations: [AuthDialogComponent, SignInComponent],
   entryComponents: [AuthDialogComponent]
 })
 export class AuthComponentsModule {
--- src/app/core/auth/auth-components/auth-dialog/auth-dialog.component.ts
+++ src/app/core/auth/auth-components/auth-dialog/auth-dialog.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { AuthDialogOptions } from '@core/auth/auth-components/auth-dialog/auth-dialog.model';
 
 @Component({
   selector: 'app-auth-dialog',
   templateUrl: './auth-dialog.component.html',
-  styleUrls: ['./auth-dialog.component.css']
+  styleUrls: ['./auth-dialog.component.scss']
 })
 export class AuthDialogComponent implements OnInit {
 
@@ -14,12 +16,16 @@ export class AuthDialogComponent implements OnInit {
   }
 
   onCancelClick() {
-
+    this.dialogRef.close();
   }
 
-  constructor() { }
-
   ngOnInit() {
+  }
+
+  constructor(
+    public dialogRef: MatDialogRef<AuthDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public options: AuthDialogOptions,
+  ) {
   }
 
 }
