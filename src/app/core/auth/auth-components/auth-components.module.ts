ed2d976c4d1a97fc873aecf45d656e5deb5a04d8
kazinov <dm.kazinov@gmail.com>
2019-07-26 14:56:26 +0200

    sign in markup

--- src/app/core/auth/auth-components/auth-components.module.ts
+++ src/app/core/auth/auth-components/auth-components.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { AuthDialogComponent } from '@core/auth/auth-components/auth-dialog/auth-dialog.component';
-import { MatButtonModule, MatDialogModule, MatIconModule, MatProgressSpinnerModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatProgressSpinnerModule
+} from '@angular/material';
 import { CommonModule } from '@angular/common';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RegisterComponent } from '@core/auth/auth-components/register/register.component';
 import { ForgotPasswordComponent } from '@core/auth/auth-components/forgot-password/forgot-password.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -13,6 +22,12 @@ import { ForgotPasswordComponent } from '@core/auth/auth-components/forgot-passw
     MatProgressSpinnerModule,
     MatButtonModule,
     MatDialogModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [],
--- src/app/core/auth/auth-components/sign-in/sign-in.component.ts
+++ src/app/core/auth/auth-components/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  @Output() forgotPassword = new EventEmitter();
 
-  constructor() { }
+  form: FormGroup;
+  emailControl: AbstractControl;
+  passwordControl: AbstractControl;
 
-  ngOnInit() {
+  private initForm() {
+    this.form = new FormGroup({});
+    this.form.registerControl('email', this.emailControl = new FormControl('',
+      [
+        Validators.required,
+        Validators.email
+      ]));
+    this.form.registerControl('password', this.passwordControl = new FormControl('',
+      [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(25),
+      ]));
   }
 
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      return;
+    }
+  }
 }
