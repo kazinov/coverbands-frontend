d38985cc383cf8646e1fbc213b09efc7161756e5
kazinov <dm.kazinov@gmail.com>
2019-09-25 17:04:34 +0200

    spinner

--- src/app/admin/artist-admin/artist-list/artist-list.component.ts
+++ src/app/admin/artist-admin/artist-list/artist-list.component.ts
@@ -4,6 +4,9 @@ import { FormBuilder } from '@angular/forms';
 import { select, Store } from '@ngrx/store';
 import { Actions } from '@ngrx/effects';
 import {
+  deleteArtistAction,
+  deleteArtistFailureAction,
+  deleteArtistSuccessAction,
   loadArtistsAction,
   loadArtistsFailureAction,
   loadArtistsSuccessAction,
@@ -11,11 +14,13 @@ import {
 } from '@core/artist/artist.actions';
 import { ArtistListSelectors } from '@artist-admin/artist-list/artist-list.selectors';
 import { AuthSelectors } from '@core/auth/auth.selectors';
-import { delay, filter, take, tap } from 'rxjs/operators';
+import { delay, distinctUntilChanged, filter, map, take, tap } from 'rxjs/operators';
 import { Artist } from '@core/artist/artist.model';
 import { getIsLoadingObservable } from '@shared/utils/get-is-loading-observable';
 import { untilDestroyed } from 'ngx-take-until-destroy';
 import { deleteArtistWithConfirmation } from '@artist-admin/artist-admin.actions';
+import { combineLatest } from 'rxjs';
+import { anyBooleanTrue } from '@shared/utils/any-boolean-true';
 
 @Component({
   selector: 'app-artist-list',
@@ -40,6 +45,29 @@ export class ArtistListComponent implements OnInit, OnDestroy {
       takeUntil: untilDestroyed(this)
     }
   );
+
+  isDeleting$ = getIsLoadingObservable(
+    this.actions$,
+    {
+      startActions: [
+        deleteArtistAction
+      ],
+      stopActions: [
+        deleteArtistSuccessAction,
+        deleteArtistFailureAction
+      ],
+      takeUntil: untilDestroyed(this)
+    }
+  );
+
+  isSpinner$ = combineLatest(
+    this.isLoading$,
+    this.isDeleting$
+  )
+    .pipe(
+      map(anyBooleanTrue),
+      distinctUntilChanged()
+    );
 
   constructor(
     private formBuilder: FormBuilder,
